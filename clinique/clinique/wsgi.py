d92fc9e188c33b374520a6ae1727f00fab42cf6a
Hubert Danfaga <hubertdanfaga383@gmail.com>
2026-08-09 11:14:22 +0000

    Auto create admin user in wsgi

--- clinique/clinique/wsgi.py
+++ clinique/clinique/wsgi.py
@@ -4,13 +4,22 @@ WSGI config for clinique project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinique.settings')
 
 application = get_wsgi_application()
+
+# Création automatique du compte administrateur au démarrage du serveur Render
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'MonMotDePasse123!')
+        print("--> Compte administrateur 'admin' créé avec succès !")
+except Exception as e:
+    print(f"Erreur lors de la création du compte admin : {e}")

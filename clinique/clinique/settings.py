2843180b95f24c8f65cc60bd2511a06fe7345f72
Hubert Danfaga <hubertdanfaga383@gmail.com>
2026-08-09 12:33:41 +0000

    Activation de la base PostgreSQL pour sauvegarder les donnees

--- clinique/clinique/settings.py
+++ clinique/clinique/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -95,10 +96,10 @@ WSGI_APPLICATION = 'clinique.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
